e0e52a0eff0a7495fdd9e98b36659f28b1addc55
Mateus Cavedini Pereira <mateuscavedini@gmail.com>
2023-12-13 11:18:32 -0300

    Adicionado endpoint para detalhes de receita

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -2,6 +2,6 @@ class Api::V1::RecipesController < Api::V1::ApiController
   def show
     recipe = Recipe.find(params[:id])
 
-    render status: 200, json: recipe
+    render status: 200, json: { recipe: recipe, user: { email: recipe.user.email } }
   end
 end
--- spec/apis/v1/user_view_recipe_details_spec.rb
+++ spec/apis/v1/user_view_recipe_details_spec.rb
@@ -1,13 +1,43 @@
 require 'rails_helper'
 
-describe 'Usuário vê detalhes de receita' do
+describe 'Usuário vê detalhes de receita', type: :request do
   context 'GET /api/v1/recipes/:id' do
     it 'com sucesso' do
-      User.create!()
+      user = User.create!(email: 'user@email.com', password: 'senha123')
+      recipe_type = RecipeType.create!(name: 'Sobremesa')
+      recipe = Recipe.create!(title: 'Brigadeiro', cook_time: 30, ingredients: 'Leite condensado, chocolate em pó e manteiga', instructions: 'Misture tudo, leve ao fogo e mexa até começar a soltar da panela. Desligue e espere esfriar.', recipe_type: recipe_type, user: user)
+
+      get "/api/v1/recipes/#{recipe.id}"
+      response_json = JSON.parse(response.body)
+
+      expect(response.status).to eq 200
+      expect(response_json['recipe']['title']).to eq 'Brigadeiro'
+      expect(response_json['recipe']['ingredients']).to eq 'Leite condensado, chocolate em pó e manteiga'
+      expect(response_json['recipe']['instructions']).to eq 'Misture tudo, leve ao fogo e mexa até começar a soltar da panela. Desligue e espere esfriar.'
+      expect(response_json['recipe']['cook_time']).to eq 30
+      expect(response_json['user']['email']).to eq 'user@email.com'
     end
   
-    pending 'mas receita não existe'
+    it 'mas receita não existe' do
+      get '/api/v1/recipes/1'
+      response_json = JSON.parse(response.body)
+
+      expect(response.status).to eq 404
+      expect(response_json['message']).to eq 'Recurso não encontrado.'
+    end
   
-    pending 'com erro interno'
+    it 'com erro interno' do
+      user = User.create!(email: 'user@email.com', password: 'senha123')
+      recipe_type = RecipeType.create!(name: 'Sobremesa')
+      recipe = Recipe.create!(title: 'Brigadeiro', cook_time: 30, ingredients: 'Leite condensado, chocolate em pó e manteiga', instructions: 'Misture tudo, leve ao fogo e mexa até começar a soltar da panela. Desligue e espere esfriar.', recipe_type: recipe_type, user: user)
+
+      allow(Recipe).to receive(:find).and_raise(ActiveRecord::QueryCanceled)
+
+      get "/api/v1/recipes/#{recipe.id}"
+      response_json = JSON.parse(response.body) 
+
+      expect(response.status).to eq 500
+      expect(response_json['message']).to eq 'Erro interno no servidor.'
+    end
   end
 end
